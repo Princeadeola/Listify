8949d8324ada52b04de49b38696c777775caf1c2
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-14 21:29:58 +0000

    Committed

---
+++ app/src/main/java/com/adeolaadesipe/listify/MainActivity.java
@@ -0,0 +1,14 @@
+package com.adeolaadesipe.listify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
