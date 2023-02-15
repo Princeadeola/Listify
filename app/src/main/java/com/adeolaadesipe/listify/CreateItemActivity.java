3c3f2c91a0d78ebe2f4931ba312adfa9ab04ae9f
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-15 18:45:38 +0000

    committed

---
+++ app/src/main/java/com/adeolaadesipe/listify/CreateItemActivity.java
@@ -0,0 +1,14 @@
+package com.adeolaadesipe.listify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_item);
+    }
+}
