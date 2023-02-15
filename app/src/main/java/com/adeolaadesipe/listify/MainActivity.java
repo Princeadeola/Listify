c1705bd6a22922a77227d7ce231a971786b0796b
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-15 18:45:45 +0000

    committed

--- app/src/main/java/com/adeolaadesipe/listify/MainActivity.java
+++ app/src/main/java/com/adeolaadesipe/listify/MainActivity.java
@@ -2,13 +2,27 @@ package com.adeolaadesipe.listify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton createItemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createItemBtn = findViewById(R.id.addItemFabID);
+        createItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateItemActivity.class));
+                finish();
+            }
+        });
     }
 }
